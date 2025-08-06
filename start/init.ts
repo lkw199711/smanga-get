c7b1a39692daea87bfd56cfb78cc92626f7381cf
lkw199711 <lkw199711@163.com>
2025-08-06 15:21:56 +0800

    feat: toomics分离cron任务到不同时段.

--- start/init.ts
+++ start/init.ts
@@ -12,6 +12,12 @@ import ToomicsDayUpdate from '#services/toomics-update'
 import fs from 'fs'
 import OmegaScansUpdate from '#services/omegascans-update'
 let subsribeCron: any = { stop: () => { } }
+let toomicsScAllCoversCron: any = { stop: () => { } }
+let toomicsTcAllCoversCron: any = { stop: () => { } }
+let toomicsScUpdateCron: any = { stop: () => { } }
+let toomicsTcUpdateCron: any = { stop: () => { } }
+
+const crons = [subsribeCron, toomicsScAllCoversCron, toomicsTcAllCoversCron, toomicsScUpdateCron, toomicsTcUpdateCron];
 
 const linuxStr = get_os() === 'Linux' ? '/' : ''
 const dataPath = linuxStr + 'data/'
@@ -55,15 +61,13 @@ export function create_scan_cron() {
     const config = get_config().cron;
     if (!config?.enable) return;
     // 停止旧扫描任务
-    subsribeCron.stop()
+    crons.forEach(cron => {
+        if (cron && cron.stop) { cron.stop() }
+    })
     // 获取配置
     const scanInterval = config.interval || "0 0 2,14 * * *" // 每天0点和12点执行一次
     // 定时扫描任务
     subsribeCron = cron.schedule(scanInterval, async () => {
-        await new ToomicsAll('sc').start()
-        await new ToomicsAll('tc').start()
-        await new ToomicsDayUpdate('sc').start()
-        await new ToomicsDayUpdate('tc').start()
         await new OmegaScansUpdate({}).start();
         const subsribe = subscribe_read()
         for (let i = 0; i < subsribe.length; i++) {
@@ -71,6 +75,26 @@ export function create_scan_cron() {
             mangaTask.add(item)
         }
     });
+
+    // Toomics 全部封面扫描任务
+    toomicsScAllCoversCron = cron.schedule(config.cron?.toomicsScAllCoversInterval, async () => { 
+        await new ToomicsAll('sc').start()
+    })
+
+    // Toomics 全部封面扫描任务
+    toomicsTcAllCoversCron = cron.schedule(config.cron?.toomicsTcAllCoversInterval, async () => {
+        await new ToomicsAll('tc').start()
+    })
+
+    // Toomics 更新扫描任务
+    toomicsScUpdateCron = cron.schedule(config.cron?.toomicsScUpdateInterval, async () => {
+        await new ToomicsDayUpdate('sc').start()
+    })
+
+    // Toomics 更新扫描任务
+    toomicsTcUpdateCron = cron.schedule(config.cron?.toomicsTcUpdateInterval, async () => {
+        await new ToomicsDayUpdate('tc').start()
+    })
 }
 
 export function task_allocation() {
