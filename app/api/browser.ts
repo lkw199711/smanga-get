7e18a9acd90b55b253e90c0235d328accf029297
lkw199711 <lkw199711@163.com>
2026-06-26 23:47:02 +0800

    fix: headless新模式1.

--- app/api/browser.ts
+++ app/api/browser.ts
@@ -219,8 +219,6 @@ class UseBrowser {
         '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
-        '--headless=new',
-        '--disable-gpu',
         '--lang=zh-CN,zh',
         ...this.proxyArgs,
       ],
